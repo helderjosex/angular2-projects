6d3e18ee0c43666abf90e9850719cfb06c974804
Hélder José <helderjosex@gmail.com>
2016-12-18 17:23:56 -0300

    Pipes Puro/Impuro Filtros de Arrays

--- curso-angular2/_06-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
+++ curso-angular2/_06-pipes/src/app/exemplos-pipes/exemplos-pipes.component.ts
@@ -16,6 +16,29 @@ export class ExemplosPipesComponent implements OnInit {
     url: 'http://a.co/2UXVy5g'
   }
 
+  livros: string[] = ['Java', 'Angular 2'];
+
+  filtro: string;
+
+  addCurso(valor){
+    this.livros.push(valor);
+    console.log(this.livros);
+  }
+
+  obterCursos(){
+    
+    if(this.livros.length === 0 || this.filtro === undefined || this.filtro.trim()===''){
+      return this.livros;
+    }
+    return this.livros.filter((v) => {
+      if(v.toLocaleLowerCase().indexOf(this.filtro) >= 0){
+        return true;
+      }
+      return false;
+    });
+
+  }
+
   constructor() { }
 
   ngOnInit() {
