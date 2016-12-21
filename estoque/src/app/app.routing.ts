372a3348a4ffc18feca45c81f4e00c2ba12f392b
Hélder José <helderjosex@gmail.com>
2016-12-20 22:26:19 -0300

    Adicionado navbar materialize

--- estoque/src/app/app.routing.ts
+++ estoque/src/app/app.routing.ts
@@ -1,11 +1,13 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
 import { LoginGuard } from './login/login.guard';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
+    { path: 'users', component: UsersComponent, canActivate: [LoginGuard]  },
     { path: '', component: HomeComponent, canActivate: [LoginGuard] },
 
     // otherwise redirect to home
