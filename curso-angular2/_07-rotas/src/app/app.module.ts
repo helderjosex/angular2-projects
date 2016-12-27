470a3bf1e4aa452487bec3b3c65e96fdd4907541
Hélder José <helderjosex@gmail.com>
2016-12-27 18:28:34 -0300

    Rotas: Criando modulo de rotas

--- curso-angular2/_07-rotas/src/app/app.module.ts
+++ curso-angular2/_07-rotas/src/app/app.module.ts
@@ -9,7 +9,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
-import { routing } from './app.routing';
+//import { routing } from './app.routing';
+import { AppRoutingModule } from './app.routing.module';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosService } from './cursos/cursos.service';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
@@ -28,7 +29,8 @@ import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-en
     FormsModule,
     HttpModule,
     MaterializeModule,
-    routing
+    //routing
+    AppRoutingModule
   ],
   providers: [CursosService],
   bootstrap: [AppComponent]
